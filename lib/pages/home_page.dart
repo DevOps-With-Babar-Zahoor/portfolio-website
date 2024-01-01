58750dfeaf1f3c47ca465c47f2c59a13f3f65142
Naveed Abdul Sattar <49361370+naveed-a-sattar@users.noreply.github.com>
2024-01-01 22:09:44 +0000

    updated

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,41 +12,67 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: CustomColor.scaffoldBg,
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-        // Main
-        Container(
-          height: 500, 
-          width: double.maxFinite, 
-          child: Row(
-            children: [
-              Text("AK"), 
-              TextButton(
-                onPressed: (){}, 
-                child: Text("button"),
+        backgroundColor: CustomColor.scaffoldBg,
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+            // Main
+            Container(
+              height: 60,
+              margin: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 20,
               ),
-            ],
-          ),
-        ),
-        // Skills
-        Container(
-          height: 500, width: double.maxFinite, color: Colors.blueGrey,
-        ),
-        // PROJECTS
-        Container(
-          height: 500, width: double.maxFinite, 
-        ),
-        // Contact
-        Container(
-          height: 500, width: double.maxFinite, color: Colors.blueGrey,
-        ),
-        // Footer
-        Container(
-          height: 500, width: double.maxFinite,
-        ),
-      ],)
-    );
+              width: double.maxFinite,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.transparent,
+                    CustomColor.bgLight1,
+                  ],
+                ),
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: Row(
+                children: [
+                  Text("AK"),
+                  Spacer(),
+                  for (int i = 0; i < 5; i++)
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        right: 20,
+                      ),
+                      child: TextButton(
+                        onPressed: () {},
+                        child: const Text("button"),
+                      ),
+                    ),
+                ],
+              ),
+            ),
+            // Skills
+            Container(
+              height: 500,
+              width: double.maxFinite,
+              color: Colors.blueGrey,
+            ),
+            // PROJECTS
+            Container(
+              height: 500,
+              width: double.maxFinite,
+            ),
+            // Contact
+            Container(
+              height: 500,
+              width: double.maxFinite,
+              color: Colors.blueGrey,
+            ),
+            // Footer
+            Container(
+              height: 500,
+              width: double.maxFinite,
+            ),
+          ],
+        ));
   }
 }
